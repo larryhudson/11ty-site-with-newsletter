14d5714d8d9b1c6570bb8e4145f5743b5d529a29
Larry Hudson <larryhudson@hey.com>
2022-04-06 08:08:14 +1000

    clean up and upgrade mjml plugin

--- .eleventy.js
+++ .eleventy.js
@@ -10,28 +10,19 @@ module.exports = function (eleventyConfig) {
         const endDate = dayjs(date);
         const startDate = endDate.subtract(days, "day");
 
-        console.log(items);
-
         dayjs.extend(isBetween);
 
         return items.filter((item) =>
           dayjs(item.data.date).isBetween(endDate, startDate)
         );
-
-        // get the start date
-
-        return value;
       },
     },
   });
 
   eleventyConfig.addCollection("newsletterArticles", function (collectionApi) {
-    const newsletterArticles = collectionApi.getAll().filter((item) => {
+    return collectionApi.getAll().filter((item) => {
       return "newsletterBlurb" in item.data;
     });
-
-    console.log(newsletterArticles);
-    return newsletterArticles;
   });
 
   return {
